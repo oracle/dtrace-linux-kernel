9c63f12fd7e96948a00dfd658088aca59b006b31
Kris Van Hees <kris.van.hees@oracle.com>
2018-08-22 12:05:26 -0400

    dtrace: FBT entry probes for arm64
    This commit provides an implementation of DTrace FBT probes for arm64.
    The actual probe support is currently limited to FBT entry probes, as a
    first phase.  Probes are fired using a breakpoint that is tagged as a
    DTrace FBT breakpoint (similar to DTrace SDT probes).  To this end, the
    breakpoint use for DTrace is being modified in this commit also, to be
    able to distinguish between SDT and FBT related breakpoints.
    
    Because FBT ends up sharing some functionality with SDT (beyond the use
    of breakpoints), the functions dtrace_sdt_peek, dtrace_sdt_poke,
    dtrace_sdt_start, and dtrace_sdt_stop are being replaced by generic
    versions that can support both SDT and FBT.
    
    In order for FBT probes to be able to function, a change is made in the
    trap handling code for breakpoints.  If the ESR code indicates that the
    breakpoint is a DTrace FBT breakpoint, we emulate a "mov x29, sp"
    instruction because that is the one that got replaced by the breakpoint.
    This change does not affect anything beyond FBT related breakpoints.
    
    Orabug: 28643859
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Reviewed-by: Nick Alcock <nick.alcock@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- arch/arm64/include/asm/dtrace_util.h
+++ arch/arm64/include/asm/dtrace_util.h
@@ -6,4 +6,9 @@
 
 #include <asm/dtrace_arch.h>
 
+extern asm_instr_t dtrace_text_peek(asm_instr_t *addr);
+extern void dtrace_text_poke(asm_instr_t *addr, asm_instr_t opcode);
+extern void dtrace_brk_start(void *arg);
+extern void dtrace_brk_stop(void *arg);
+
 #endif /* _ASM_ARM64_DTRACE_UTIL_H */
