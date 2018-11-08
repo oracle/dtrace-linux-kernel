95775ebc9ce0c636173401a2866307f6928fa60c
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-08 18:57:33 +0000

    dtrace, arm: arm64 port
    This provides an arm64 implementation of DTrace.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ arch/arm64/kernel/dtrace_sdt.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * FILE:        dtrace_sdt.c
+ * DESCRIPTION: Dynamic Tracing: SDT registration code (arch-specific)
+ *
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ */
+
+#include <linux/module.h>
+#include <asm/cacheflush.h>
+#include <asm/dtrace_arch.h>
+#include <asm/dtrace_sdt_arch.h>
+
+void __init_or_module dtrace_sdt_nop_multi(asm_instr_t **addrs,
+					   int * __always_unused is_enabled,
+					   int cnt)
+{
+	int		i;
+	asm_instr_t	*addr;
+
+	for (i = 0; i < cnt; i++) {
+		addr = addrs[i];
+		*addr = NOP_INSTR;
+		flush_icache_range((uintptr_t)addr, (uintptr_t)(addr + 1));
+	}
+}
