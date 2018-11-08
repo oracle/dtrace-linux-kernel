16ad662d7d2a25ccccad4723a4c4ebd580573041
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-08 18:57:33 +0000

    dtrace, arm: arm64 port
    This provides an arm64 implementation of DTrace.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ arch/arm64/include/asm/dtrace_cpuinfo.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2018 Oracle, Inc. */
+
+#ifndef _ASM_ARM64_DTRACE_CPUINFO_H_
+#define _ASM_ARM64_DTRACE_CPUINFO_H_
+
+#include <asm/cpu.h>
+
+typedef struct cpuinfo_arm64		cpuinfo_arch_t;
+
+#define dtrace_cpuinfo_chip(ci)		((ci)->cpu.node_id)
+
+#endif /* _ASM_ARM64_DTRACE_CPUINFO_H_ */
