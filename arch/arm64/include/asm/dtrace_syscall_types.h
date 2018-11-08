12a7df186ee3e5a6c6b288136f15da5ac35ba93d
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-08 18:57:33 +0000

    dtrace, arm: arm64 port
    This provides an arm64 implementation of DTrace.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ arch/arm64/include/asm/dtrace_syscall_types.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ */
+
+#include <linux/types.h>
+#include <linux/dtrace_types.h>
+
+typedef asmlinkage long (*dt_sys_call_t)(const struct pt_regs *regs);
+
+#define DTRACE_SYSCALL_WRAP_PREFIX "__arm64_"
