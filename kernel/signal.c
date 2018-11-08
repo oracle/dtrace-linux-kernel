f7b8a79775bd4425e50e8807b48261ed6be44076
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-08 18:59:39 +0000

    dtrace: add SDT probes
    This adds a variety of SDT probes.
    
    XXX add documentation here from the commit messages
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -48,6 +48,7 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
+#include <linux/sdt.h>
 
 #include <asm/param.h>
 #include <linux/uaccess.h>
@@ -1090,8 +1091,12 @@ static int __send_signal(int sig, struct kernel_siginfo *info, struct task_struc
 
 	result = TRACE_SIGNAL_IGNORED;
 	if (!prepare_signal(sig, t,
-			from_ancestor_ns || (info == SEND_SIG_PRIV)))
+			from_ancestor_ns || (info == SEND_SIG_PRIV))) {
+		DTRACE_PROC(signal__discard,
+			    struct task_struct * : (lwpsinfo_t *, psinfo_t *), t,
+			    int, sig);
 		goto ret;
+	}
 
 	pending = (type != PIDTYPE_PID) ? &t->signal->shared_pending : &t->pending;
 	/*
@@ -1192,6 +1197,9 @@ out_set:
 	}
 
 	complete_signal(sig, t, type);
+	DTRACE_PROC(signal__send,
+		     struct task_struct * : (lwpsinfo_t *, psinfo_t *), t,
+		     int, sig);
 ret:
 	trace_signal_generate(sig, info, t, type != PIDTYPE_PID, result);
 	return ret;
@@ -1795,6 +1803,9 @@ int send_sigqueue(struct sigqueue *q, struct pid *pid, enum pid_type type)
 	list_add_tail(&q->list, &pending->list);
 	sigaddset(&pending->signal, sig);
 	complete_signal(sig, t, type);
+	DTRACE_PROC(signal__send,
+		    struct task_struct * : (lwpsinfo_t *, psinfo_t *), t,
+		    int, sig);
 	result = TRACE_SIGNAL_DELIVERED;
 out:
 	trace_signal_generate(sig, &q->info, t, type != PIDTYPE_PID, result);
@@ -2488,6 +2499,11 @@ relock:
 		trace_signal_deliver(SIGKILL, SEND_SIG_NOINFO,
 				&sighand->action[SIGKILL - 1]);
 		recalc_sigpending();
+		DTRACE_PROC(signal__handle,
+			    int, signal->group_exit_code
+						? signal->group_exit_code
+						: signr,
+			    siginfo_t *, NULL, void (*)(void), NULL);
 		goto fatal;
 	}
 
@@ -2539,6 +2555,15 @@ relock:
 		}
 
 		ka = &sighand->action[signr-1];
+
+		DTRACE_PROC(signal__handle,
+			    int, signal->group_exit_code
+						? signal->group_exit_code
+						: signr,
+			    siginfo_t *, ksig->ka.sa.sa_handler != SIG_DFL
+						? NULL
+						: &ksig->info,
+			    void (*)(void), ksig->ka.sa.sa_handler);
 
 		/* Trace actually delivered signals. */
 		trace_signal_deliver(signr, &ksig->info, ka);
@@ -3411,8 +3436,10 @@ static int do_sigtimedwait(const sigset_t *which, kernel_siginfo_t *info,
 	}
 	spin_unlock_irq(&tsk->sighand->siglock);
 
-	if (sig)
+	if (sig) {
+		DTRACE_PROC(signal__clear, int, sig);
 		return sig;
+	}
 	return ret ? -EINTR : -EAGAIN;
 }
 
