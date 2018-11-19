5ce515f2de01f4f2642707587c0df091870dd8ff
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 22:03:10 +0000

    dtrace: sdt provider core components
    This implements the core (linked-in) machinery needed for SDT
    tracepoints:
    
     - generate empty stub function calls __dtrace_probe_* for each probe
       point and perf-event probe point, and record their section-relative
       offset in tables in special symbols in the output; calls to
       is-enabling probes (conditionals of the form
       if (DTRACE_FOO_ENABLED(probe-name))) are translated as well
     - similarly record the names and types of arguments to probes in
       special sections
     - parse both of these at load time, and substitute in nops over the top
       of the stub functions, remembering their locations: is-enabled probes
       get 0-returns patched over the top
     - on probe enabling, patch invalid-operation traps over the top of
       those stub functions; handle these by calling the probe, then return
       as if the trap had never happened
    
    The provider module itself is added in the next commit.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

--- arch/x86/include/asm/dtrace_arch.h
+++ arch/x86/include/asm/dtrace_arch.h
@@ -12,16 +12,22 @@
 
 typedef uint8_t		asm_instr_t;
 
+#define ASM_CALL_SIZE			5
+
 typedef int (*prov_exit_f)(void);
 
 /*
  * Structure to hold DTrace specific information about modules (including the
  * core kernel module).  Note that each module (and the main kernel) already
- * has one field that relates to probing:
+ * has three fields that relate to probing:
+ *	- sdt_probes: description of SDT probes in the module
+ *	- sdt_probec: number of SDT probes in the module
  *	- pdata: pointer to a dtrace_module struct (for DTrace)
  */
 struct dtrace_module {
 	int             enabled_cnt;
+	size_t          sdt_probe_cnt;
+	size_t          fbt_probe_cnt;
 	prov_exit_f	prov_exit;	/* Called with module_mutex held */
 };
 
