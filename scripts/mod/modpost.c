f653bdb8cb35fc0f1313c89d8b9a6e8e7e375fa9
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 22:03:10 +0000

    dtrace: sdt provider core components
    This implements the core (linked-in) machinery needed for SDT
    tracepoints:
    
     - generate empty stub function calls __dtrace_probe_* for each probe
       point and perf-event probe point, and record their section-relative
       offset in tables in special symbols in the output; calls to
       is-enabling probes (conditionals of the form
       if (DTRACE_FOO_ENABLED(probe-name))) are translated as well
     - similarly record the names and types of arguments to probes in
       special sections
     - parse both of these at load time, and substitute in nops over the top
       of the stub functions, remembering their locations: is-enabled probes
       get 0-returns patched over the top
     - on probe enabling, patch invalid-operation traps over the top of
       those stub functions; handle these by calling the probe, then return
       as if the trap had never happened
    
    The provider module itself is added in the next commit.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -2156,7 +2156,9 @@ static void check_exports(struct module *mod)
 		const char *basename;
 		exp = find_symbol(s->name);
 		if (!exp || exp->module == mod) {
-			if (!s->weak && nr_unresolved++ < MAX_UNRESOLVED_REPORTS)
+			if (!s->weak && nr_unresolved++ < MAX_UNRESOLVED_REPORTS &&
+			    !strstarts(s->name, "__dtrace_probe_") &&
+			    !strstarts(s->name, "__dtrace_isenabled_"))
 				modpost_log(warn_unresolved ? LOG_WARN : LOG_ERROR,
 					    "\"%s\" [%s.ko] undefined!\n",
 					    s->name, mod->name);
@@ -2199,6 +2201,13 @@ static void check_modname_len(struct module *mod)
  **/
 static void add_header(struct buffer *b, struct module *mod)
 {
+	const char *modname;
+
+	modname = strrchr(mod->name, '/');
+	if (modname != NULL)
+		modname++;
+	else
+		modname = mod->name;
 	buf_printf(b, "#include <linux/module.h>\n");
 	/*
 	 * Include build-salt.h after module.h in order to
@@ -2209,6 +2218,10 @@ static void add_header(struct buffer *b, struct module *mod)
 	buf_printf(b, "#include <linux/elfnote-lto.h>\n");
 	buf_printf(b, "#include <linux/vermagic.h>\n");
 	buf_printf(b, "#include <linux/compiler.h>\n");
+	buf_printf(b, "\n");
+	buf_printf(b, "#ifdef CONFIG_DTRACE\n");
+	buf_printf(b, "# include \"%s.sdtinfo.h\"\n", modname);
+	buf_printf(b, "#endif\n");
 	buf_printf(b, "\n");
 	buf_printf(b, "BUILD_SALT;\n");
 	buf_printf(b, "BUILD_LTO_INFO;\n");
@@ -2226,6 +2239,10 @@ static void add_header(struct buffer *b, struct module *mod)
 			      "\t.exit = cleanup_module,\n"
 			      "#endif\n");
 	buf_printf(b, "\t.arch = MODULE_ARCH_INIT,\n");
+	buf_printf(b, "#ifdef CONFIG_DTRACE\n");
+	buf_printf(b, "\t.sdt_probes = _sdt_probes,\n");
+	buf_printf(b, "\t.sdt_probec = _sdt_probec,\n");
+	buf_printf(b, "#endif\n");
 	buf_printf(b, "};\n");
 }
 
