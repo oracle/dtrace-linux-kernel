bce97b41fdc56a484cf80d29b943692d991e8f62
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 18:08:41 +0000

    dtrace: USDT and pid providers
    For historical reasons, these are provided in a module named
    fasttrap.ko.
    
    Much of this is arch-dependent code for jump-table detection and
    implementation of globbed pid probes ("probe everything you can in this
    function"), as well as arch-dependent code to look up arguments and code
    to ensure that dropping a kprobe in a single process does not affect
    other processes running from the same binary.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ dtrace/fasttrap_mod.c
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * FILE:	fasttrap_mod.c
+ * DESCRIPTION:	DTrace - fasttrap provider kernel module
+ *
+ * Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+
+#define DTRACE_HAVE_PROV_EXIT
+
+#include "dtrace.h"
+#include "dtrace_dev.h"
+#include "fasttrap_impl.h"
+
+MODULE_AUTHOR("Kris Van Hees (kris.van.hees@oracle.com)");
+MODULE_DESCRIPTION("Fasttrap Tracing");
+MODULE_VERSION("v0.1");
+MODULE_LICENSE("GPL");
+
+struct dtrace_mops fasttrap_mops = {
+	fasttrap_meta_create_probe,
+	fasttrap_meta_provide,
+	fasttrap_meta_remove
+};
+
+DT_META_PROVIDER_MODULE(fasttrap)
