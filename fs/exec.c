52a4a4fb55f37d97caddac7181fd6cd07adfb666
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 22:21:36 +0000

    dtrace: core and x86
    This implements DTrace's core kernel (linked-in) components,
    including platform-dependent portions for x86.  (Most of this
    machinery is not used until the next commit.)
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

--- fs/exec.c
+++ fs/exec.c
@@ -64,6 +64,7 @@
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
+#include <linux/dtrace_os.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1822,6 +1823,10 @@ static int bprm_execve(struct linux_binprm *bprm,
 		goto out;
 
 	/* execve succeeded */
+
+	/* Update DTrace per-task data. */
+	dtrace_task_exec(current);
+
 	current->fs->in_exec = 0;
 	current->in_execve = 0;
 	rseq_execve(current);
