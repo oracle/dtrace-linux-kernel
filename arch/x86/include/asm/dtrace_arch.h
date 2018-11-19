5d874a4ff379800c9b302dc70b23c04ec76c3142
Kris Van Hees <kris.van.hees@oracle.com>
2018-11-19 22:21:36 +0000

    dtrace: core and x86
    This implements DTrace's core kernel (linked-in) components,
    including platform-dependent portions for x86.  (Most of this
    machinery is not used until the next commit.)
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Signed-off-by: Eugene Loh <eugene.loh@oracle.com>
    Signed-off-by: David Mc Lean <david.mclean@oracle.com>
    Signed-off-by: Vincent Lim <vincent.lim@oracle.com>

---
+++ arch/x86/include/asm/dtrace_arch.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ */
+
+#ifndef _X86_DTRACE_ARCH_H
+#define _X86_DTRACE_ARCH_H
+
+/* Number of arguments stored inside the mstate. */
+#define	DTRACE_MSTATE_ARGS_MAX		6
+
+typedef uint8_t		asm_instr_t;
+
+typedef int (*prov_exit_f)(void);
+
+/*
+ * Structure to hold DTrace specific information about modules (including the
+ * core kernel module).  Note that each module (and the main kernel) already
+ * has one field that relates to probing:
+ *	- pdata: pointer to a dtrace_module struct (for DTrace)
+ */
+struct dtrace_module {
+	int             enabled_cnt;
+	prov_exit_f	prov_exit;	/* Called with module_mutex held */
+};
+
+#endif /* _X86_DTRACE_ARCH_H */
