09f606ce2f8b1f18e6c42e06a55cb2fed23113bb
Nick Alcock <nick.alcock@oracle.com>
2019-01-21 18:08:02 +0000

    dtrace: manual detypedeffing transforms
    These are transformations Coccinelle cannot do, usually because it
    cannot parse the constructs in question.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Reviewed-by: David Mc Lean <david.mclean@oracle.com>

--- include/uapi/linux/dtrace/ioctl.h
+++ include/uapi/linux/dtrace/ioctl.h
@@ -3,7 +3,7 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  *
- * Copyright (c) 2009, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
  */
 
 #ifndef _LINUX_DTRACE_IOCTL_H_
@@ -22,26 +22,26 @@
 #include <linux/dtrace/cpu_defines.h>
 
 #define DTRACEIOC		0xd4
-#define DTRACEIOC_PROVIDER	_IOR(DTRACEIOC, 1, dtrace_providerdesc_t)
-#define DTRACEIOC_PROBES	_IOR(DTRACEIOC, 2, dtrace_probedesc_t)
-#define DTRACEIOC_BUFSNAP	_IOR(DTRACEIOC, 4, dtrace_bufdesc_t)
-#define DTRACEIOC_PROBEMATCH	_IOR(DTRACEIOC, 5, dtrace_probedesc_t)
+#define DTRACEIOC_PROVIDER	_IOR(DTRACEIOC, 1, struct dtrace_providerdesc)
+#define DTRACEIOC_PROBES	_IOR(DTRACEIOC, 2, struct dtrace_probedesc)
+#define DTRACEIOC_BUFSNAP	_IOR(DTRACEIOC, 4, struct dtrace_bufdesc)
+#define DTRACEIOC_PROBEMATCH	_IOR(DTRACEIOC, 5, struct dtrace_probedesc)
 #define DTRACEIOC_ENABLE	_IOW(DTRACEIOC, 6, void *)
-#define DTRACEIOC_AGGSNAP	_IOR(DTRACEIOC, 7, dtrace_bufdesc_t)
-#define DTRACEIOC_EPROBE	_IOW(DTRACEIOC, 8, dtrace_eprobedesc_t)
-#define DTRACEIOC_PROBEARG	_IOR(DTRACEIOC, 9, dtrace_argdesc_t)
-#define DTRACEIOC_CONF		_IOR(DTRACEIOC, 10, dtrace_conf_t)
-#define DTRACEIOC_STATUS	_IOR(DTRACEIOC, 11, dtrace_status_t)
+#define DTRACEIOC_AGGSNAP	_IOR(DTRACEIOC, 7, struct dtrace_bufdesc)
+#define DTRACEIOC_EPROBE	_IOW(DTRACEIOC, 8, struct dtrace_eprobedesc)
+#define DTRACEIOC_PROBEARG	_IOR(DTRACEIOC, 9, struct dtrace_argdesc)
+#define DTRACEIOC_CONF		_IOR(DTRACEIOC, 10, struct dtrace_conf)
+#define DTRACEIOC_STATUS	_IOR(DTRACEIOC, 11, struct dtrace_status)
 #define DTRACEIOC_GO		_IOW(DTRACEIOC, 12, processorid_t)
 #define DTRACEIOC_STOP		_IOW(DTRACEIOC, 13, processorid_t)
-#define DTRACEIOC_AGGDESC	_IOR(DTRACEIOC, 15, dtrace_aggdesc_t)
-#define DTRACEIOC_FORMAT	_IOR(DTRACEIOC, 16, dtrace_fmtdesc_t)
-#define DTRACEIOC_DOFGET	_IOR(DTRACEIOC, 17, dof_hdr_t)
+#define DTRACEIOC_AGGDESC	_IOR(DTRACEIOC, 15, struct dtrace_aggdesc)
+#define DTRACEIOC_FORMAT	_IOR(DTRACEIOC, 16, struct dtrace_fmtdesc)
+#define DTRACEIOC_DOFGET	_IOR(DTRACEIOC, 17, struct dof_hdr)
 #define DTRACEIOC_REPLICATE	_IOR(DTRACEIOC, 18, void *)
 
 #define DTRACEHIOC		0xd8
-#define DTRACEHIOC_ADD		_IOW(DTRACEHIOC, 1, dof_hdr_t)
+#define DTRACEHIOC_ADD		_IOW(DTRACEHIOC, 1, struct dof_hdr)
 #define DTRACEHIOC_REMOVE	_IOW(DTRACEHIOC, 2, int)
-#define DTRACEHIOC_ADDDOF	_IOW(DTRACEHIOC, 3, dof_helper_t)
+#define DTRACEHIOC_ADDDOF	_IOW(DTRACEHIOC, 3, struct dof_helper)
 
 #endif /* _LINUX_DTRACE_IOCTL_H */
