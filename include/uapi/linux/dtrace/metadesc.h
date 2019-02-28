04f8aef65bce0687eb4a9ba9bc0524e8462b6770
Nick Alcock <nick.alcock@oracle.com>
2019-02-28 23:05:40 +0000

    dtrace: unbreak the uapi
    The detypedeffing commit broke the uapi by removing typedefs of types
    relied upon by DTrace consumers in userspace.  Put the typedefs back again.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- include/uapi/linux/dtrace/metadesc.h
+++ include/uapi/linux/dtrace/metadesc.h
@@ -33,7 +33,7 @@
  * structure.  Note that all four of these structures must be bitness-neutral
  * to allow for a 32-bit DTrace consumer on a 64-bit kernel.
  */
-struct dtrace_recdesc {
+typedef struct dtrace_recdesc {
 	dtrace_actkind_t dtrd_action;		/* kind of action */
 	uint32_t dtrd_size;			/* size of record */
 	uint32_t dtrd_offset;			/* offset in ECB's data */
@@ -41,18 +41,18 @@ struct dtrace_recdesc {
 	uint16_t dtrd_format;			/* format, if any */
 	uint64_t dtrd_arg;			/* action argument */
 	uint64_t dtrd_uarg;			/* user argument */
-};
+} dtrace_recdesc_t;
 
-struct dtrace_eprobedesc {
+typedef struct dtrace_eprobedesc {
 	dtrace_epid_t dtepd_epid;		/* enabled probe ID */
 	dtrace_id_t dtepd_probeid;		/* probe ID */
 	uint64_t dtepd_uarg;			/* library argument */
 	uint32_t dtepd_size;			/* total size */
 	int dtepd_nrecs;			/* number of records */
-	struct dtrace_recdesc dtepd_rec[1];		/* recods themselves */
-};
+	struct dtrace_recdesc dtepd_rec[1];	/* records themselves */
+} dtrace_eprobedesc_t;
 
-struct dtrace_aggdesc {
+typedef struct dtrace_aggdesc {
 	DTRACE_PTR(char, dtagd_name);		/* not filled in by kernel */
 	dtrace_aggvarid_t dtagd_varid;		/* not filled in by kernel */
 	int dtagd_flags;			/* not filled in by kernel */
@@ -61,14 +61,14 @@ struct dtrace_aggdesc {
 	uint32_t dtagd_size;			/* size in bytes */
 	int dtagd_nrecs;			/* number of records */
 	uint32_t dtagd_pad;			/* explicit padding */
-	struct dtrace_recdesc dtagd_rec[1];		/* record descriptions */
-};
+	struct dtrace_recdesc dtagd_rec[1];	/* record descriptions */
+} dtrace_aggdesc_t;
 
-struct dtrace_fmtdesc {
+typedef struct dtrace_fmtdesc {
 	DTRACE_PTR(char, dtfd_string);		/* format string */
 	int dtfd_length;			/* length of format string */
 	uint16_t dtfd_format;			/* format identifier */
-};
+} dtrace_fmtdesc_t;
 
 #define DTRACE_SIZEOF_EPROBEDESC(desc)				\
 	(sizeof(struct dtrace_eprobedesc) + ((desc)->dtepd_nrecs ?  \
